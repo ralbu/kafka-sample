d7fc47616d06cffabb62ca6e3980f0eecf6d56d2
Ruslan <ruslan.albu@hotmail.com>
2019-02-22 13:58:55 +0000

    kafka project

---
+++ Producer/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new Dictionary<string, object>
+            {
+                { "bootstrap.servers", "localhost:9092" }
+            };
+
+            using (var producer = new Producer<Null, string>(config, null, new StringSerializer(Encoding.UTF8)))
+            {
+                string text = null;
+
+                while (text != "exit")
+                {
+                    text = Console.ReadLine();
+                    producer.ProduceAsync("blog-topic", null, text);
+                }
+
+                producer.Flush(100);
+            }
+        }
+    }
+
+}
